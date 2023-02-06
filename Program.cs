cde28bf729ce2d9759dd06d9a2e4a14454577b2a
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2023-02-06 22:51:52 +0100

    adding packages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration =  new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile($"appsettings.json", optional: false);
+            
+var config = configuration.Build();
+var apiKeyValue = config.GetSection("apiKeys").Value;
+
+if(args.Length > 0)
+{
+     HttpClient client = new HttpClient();
+     client.DefaultRequestHeaders.Add("authorization", apiKeyValue);
+}
+else
+{
+    System.Console.WriteLine("--> You need to provide some input ");
+}
