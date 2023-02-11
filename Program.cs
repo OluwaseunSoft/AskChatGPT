2e5b9f97156716008b9170d22eb5e9cddbf9b0c3
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2023-02-11 14:35:01 +0100

    published

--- Program.cs
+++ Program.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
-var configuration =  new ConfigurationBuilder()
-.SetBasePath(Directory.GetCurrentDirectory())
-.AddJsonFile($"appsettings.json", optional: false);
-            
-var config = configuration.Build();
-var apiKeyValue = config.GetSection("apiKeys").Value;
 
-if(args.Length > 0)
+if (args.Length > 0)
 {
-     HttpClient client = new HttpClient();
-     client.DefaultRequestHeaders.Add("authorization", apiKeyValue);//19:17
+    HttpClient client = new HttpClient();
+    client.DefaultRequestHeaders.Add("authorization", "Bearer <INSERT YOUR API KEY HERE>");
+
+    var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100}",
+    Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+    string responseString = await response.Content.ReadAsStringAsync();
+
+    try
+    {
+        var dyData = JsonConvert.DeserializeObject<dynamic>(responseString);
+        
+        string guess = GuessCommand(dyData!.choices[0].text);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"--> My Guess At The Command Prompt Is: {guess}");
+        Console.ResetColor();
+    }
+    catch (Exception ex)
+    {
+        System.Console.WriteLine($"--> Could not deserialise the JSON: {ex.Message}");
+    }
+    //System.Console.WriteLine(responseString);
 }
 else
 {
     System.Console.WriteLine("--> You need to provide some input ");
+}
+
+static string GuessCommand(string raw)
+{
+    System.Console.WriteLine("--> GPT-3 API Returned Text!");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(raw);
+
+    var lastIndex = raw.LastIndexOf('\n');
+    string guess = raw.Substring(lastIndex + 1);
+    Console.ResetColor();
+    TextCopy.ClipboardService.SetText(guess);
+    return guess;
 }
